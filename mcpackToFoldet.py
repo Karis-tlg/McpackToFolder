300c3e9d1e33f84d6c5af47056c60a8ef7e8f50a
Karis <116365711+Karis-tlg@users.noreply.github.com>
2023-11-14 18:16:19 +0700

    Update mcpackToFoldet.py

--- mcpackToFoldet.py
+++ mcpackToFoldet.py
@@ -1,23 +1,23 @@
-import os
+import pathlib
 import zipfile
 
 # Nhập đường dẫn thư mục chứa các tệp .mcpack và .zip từ người dùng
-path = input("Enter the directory path: ")
+path = pathlib.Path(input("Enter the directory path: "))
 
 # Định nghĩa hàm để giải nén và xóa tệp .mcpack hoặc .zip
 def extract_archive(archive):
     # Xác định loại tệp (mcpack hoặc zip) dựa trên đuôi (extension)
-    file_extension = os.path.splitext(archive)[1].lower()
+    file_extension = archive.suffix
 
-    if file_extension == '.mcpack' or file_extension == '.zip':
+    if file_extension.lower() in ('.mcpack', '.zip'):
         # Giải nén tệp .mcpack hoặc .zip
         with zipfile.ZipFile(archive, 'r') as zip_ref:
             # Xác định thư mục đích cho việc giải nén
-            target_folder = os.path.join(os.path.dirname(archive), os.path.splitext(os.path.basename(archive))[0])
+            target_folder = archive.parent.joinpath(archive.stem)
             zip_ref.extractall(target_folder)
 
         # Xóa tệp .mcpack hoặc .zip sau khi giải nén
-        os.remove(archive)
+        archive.unlink()
 
         # In thông báo về việc giải nén tệp lên console
         print(f"Extracted: {archive}")
@@ -25,12 +25,12 @@ def extract_archive(archive):
         print(f"Skipped: {archive} (unsupported file type)")
 
 try:
-    # Lặp qua tất cả các tệp trong thư mục và các thư mục con
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            if file.endswith((".mcpack", ".zip")):
-                archive_path = os.path.join(root, file)
-                extract_archive(archive_path)  # Giải nén tệp .mcpack hoặc .zip
+    # Lấy danh sách tất cả các tệp trong thư mục
+    files = list(path.glob("*.mcpack")) + list(path.glob("*.zip"))
+
+    # Giải nén từng tệp trong danh sách
+    for file in files:
+        extract_archive(file)
 
     # Thông báo khi xử lý hoàn thành
     print("Processing complete.")
