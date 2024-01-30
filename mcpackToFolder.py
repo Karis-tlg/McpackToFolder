acd2347b4ce8557355c4666565aaaa004f35acf2
Karis <116365711+Karis-tlg@users.noreply.github.com>
2024-01-30 22:28:12 +0700

    Update mcpackToFolder.py

--- mcpackToFolder.py
+++ mcpackToFolder.py
@@ -1,25 +1,25 @@
-import glob
-import os
-import zipfile
+import glob, os, zipfile
 
-root = input("Enter the directory path: ")
+while True:
+  root = input("Enter the directory path: ")
+  if os.path.isdir(root):
+    break
+  else:
+    print("Please enter a valid directory path.")
+
 ext = (".zip", ".mcpack")
 
-if not os.path.exists(root):
-    print(f"Error: The specified directory path does not exist")
-    exit()
-
 for file in glob.glob(f"{root}/*", recursive=True):
-    if file.endswith(ext):
-        try:
-            with zipfile.ZipFile(file, "r") as zip:
-                folder = os.path.splitext(os.path.basename(file))[0]
-                zip.extractall(os.path.join(root, folder))              
-                print(f"Extracted: {file}")
-            os.remove(file)
-        except Exception as e:
-            print(f"An error occurred while extracting {file}: {e}")
-    else:
-        print(f"Skipped: {file} (unsupported file type)")
+  if file.endswith(ext):
+    try:
+      with zipfile.ZipFile(file, "r") as zip:
+        folder = os.path.splitext(file)[0]
+        zip.extractall(os.path.join(root, folder))
+        print(f"Extracted: {file}")
+      os.remove(file)
+    except zipfile.BadZipFile as e:
+      print(f"An error occurred while extracting {file}: {e}")
+  else:
+    print(f"Skipped: {file} (unsupported file type)")
 
 print("Processing complete.")
